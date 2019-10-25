f5332b85b6f3c3d84b4d68eb73fda1de52d9f607
Franki Yeung <ftyyeung@vt.edu>
2019-10-24 20:54:46 -0400

    Added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ServicesPageComponent } from './pages/services-page/services-page.compo
 import { NewsPageComponent } from './pages/news-page/news-page.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { LoginComponent } from './login/login.component';
     ServicesPageComponent,
     NewsPageComponent,
     ContactPageComponent,
-    LoginComponent
+    LoginComponent,
+    FooterComponent
   ],
   imports: [
     AppRoutingModule,
